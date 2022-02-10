b8fa2911f3f85dc99880944154dbb8ef3ced8d82
CappelleJess <cappellejessica.lcj@gmail.com>
2022-02-10 13:56:18 +0100

    items add

--- main.js
+++ main.js
@@ -1,26 +1,26 @@
 /* main.js */
 
 document.addEventListener("DOMContentLoaded", function() {
-    let backhair = body = eyes = eyebrows = mouth = nose = fronthair = clothes = new Image();
+    let clothes = backhair = body = eyebrows = eyes = mouth = nose = fronthair = new Image();
     let canvas = document.getElementById('canvas');
     let ctx = canvas.getContext('2d');
     let path = "items/";
 
-    clothes.src = path + "clothes/clothes" + random (3, 1);
-    backhair.src = path + "backhair/backhair" + random(4, 1);
-    body.src = path + "body/body" + random(5, 1);
-    face.src = path + "faces/face" + random(5, 1);
-    eyebrows.src = path + "eyebrows/eyebrows" + random(5, 1);
-    mouth.src = path + "mouth/mouth" + random(4, 1);
-    nose.src = path + "noses/nose" + random(5, 1);
-    fronthair.src = path + "fronthair/fronthair" + random(4, 1);
+    clothes.src = path + "clothes/clothes" + random (12, 1);
+    backhair.src = path + "backhair/backhair" + random(25, 1);
+    body.src = path + "body/body" + random(25, 1);
+    eyebrows.src = path + "eyebrows/eyebrows" + random(20, 1);
+    eyes.src = path + "eyes/eyes" + random (25, 1);
+    mouth.src = path + "mouth/mouth" + random(25, 1);
+    nose.src = path + "noses/nose" + random(4, 1);
+    fronthair.src = path + "fronthair/fronthair" + random(25, 1);
 
     window.onload = function () {
         ctx.drawImage(clothes, 0, 0);
         ctx.drawImage(backhair, 0, 0);
         ctx.drawImage(body, 0, 0);
-        ctx.drawImage(eyes, 0, 0);
         ctx.drawImage(eyebrows, 0, 0);
+        ctx.drawImage(eyes, 0, 0);
         ctx.drawImage(mouth, 0, 0);
         ctx.drawImage(nose, 0, 0);
         ctx.drawImage(fronthair, 0, 0);
