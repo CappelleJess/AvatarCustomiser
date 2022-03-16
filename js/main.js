6a0322fc057776900574a912b00c9d3f85ffa8ec
CappelleJess <cappellejessica.lcj@gmail.com>
2022-03-16 18:18:37 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
         fetch("json/assets.json").then(Response => Response.json()).then(data => {
         for (let i = 0; i < data.length; i++) {
-            console.log(data[i]);
+            console.log(data[i].gender);
             console.log(data[i].img);
             console.log(data[i].color);
         }
