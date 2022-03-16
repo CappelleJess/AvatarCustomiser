1f04199daea41fd7ef962765202e707fb264a25a
CappelleJess <cappellejessica.lcj@gmail.com>
2022-03-16 13:02:24 +0100

    gdthr

--- js/main.js
+++ js/main.js
@@ -1,35 +1,16 @@
 /* main.js v0.1 by djphil (CC-BY-NC-SA 4.0) */
 
 document.addEventListener("DOMContentLoaded", function() {
-    var queryString = location.search;
-    let params = new URLSearchParams(queryString);
-    let gender = parseInt(params.get('gender'));
-    let gender = params.get('gender').toString();
-    let gender = params.get('gender');
-    console.log(gender);
-
-        fetch("json/assets.json").then(Response => Response.json()).then(data => {
-        for (let i = 0; i < data.length; i++) {
-            console.log(data[i]);
-            console.log(data[i].img);
-            console.log(data[i].color);
-        }
-    });
-
     let canvas = document.getElementById('canvas');
+    let select_girl = document.getElementById('select_girl');
+    let select_boy = document.getElementById('select_boy');
 
     if (canvas.getContext) {
-        const ctx = canvas.getContext('2d');
-        const download_btn = document.getElementById('download_btn');
-        const background_btn = document.getElementById('background_btn');
-        const colors = ["white", "lightyellow", "lightcoral", "lightblue", "lightpink", "lightgreen"];
-        const path = "img/";
-
-        const query = window.location.search;
-        const param = new URLSearchParams(query);
-        let gender = param.get('gender');
-        if (gender != 'male') gender = 'female';
-        gender = gender +  "/";
+        let ctx = canvas.getContext('2d');
+        let download_btn = document.getElementById('download_btn');
+        let background_btn = document.getElementById('background_btn');
+        let colors = ["lightcoral", "lightblue", "lightpink", "lightgreen"];
+        let path = "img/";
 
         let matched = get_random_image(35, 1);
         let backhair_nbr = matched;
@@ -55,22 +36,35 @@ document.addEventListener("DOMContentLoaded", function() {
         backhair_img.crossOrigin = "anonymous";
         body_img.crossOrigin = "anonymous";
         clothes_img.crossOrigin = "anonymous";
-        eyebrows_img.crossOrigin = "anonymous";
-        eyes_img.crossOrigin = "anonymous";
-        nose_img.crossOrigin = "anonymous";
-        mouth_img.crossOrigin = "anonymous";
-        accessories_img.crossOrigin = "anonymous";
-        fronthair_img.crossOrigin = "anonymous";        
+ 
 
-        backhair_img.src = path + gender + "backhairs/backhair" + backhair_nbr;
-        body_img.src = path + "both/bodys/body" + body_nbr;
-        clothe_img.src = path + "both/clothes/clothes" + clothe_nbr;
-        eyebrow_img.src = path + "both/eyebrows/eyebrows" + eyebrow_nbr;
-        eye_img.src = path + gender + "eyes/eyes" + eye_nbr;
-        mouth_img.src = path + gender + "mouths/mouth" + mouth_nbr;
-        nose_img.src = path + "both/noses/nose" + nose_nbr;
-        accessorie_img.src = path + "both/accessories/accessories" + accessorie_nbr; 
-        fronthair_img.src = path + gender + "fronthairs/fronthair" + fronthair_nbr;
+        function Select() {
+            
+            let select_girl = document.getElementById('select_girl');
+            let select_boy = document.getElementById('select_boy');
+            
+            if (select_girl.onclick == true) {
+                backhair_img.src = path + "backhair/woman/backhair" + backhair_nbr;
+                body_img.src = path + "body/body" + body_nbr;
+                clothes_img.src = path + "clothes/clothes" + clothes_nbr;
+                eyebrows_img.src = path + "eyebrows/eyebrows" + eyebrows_nbr;
+                eyes_img.src = path + "eyes/woman/eyes" + eyes_nbr;
+                mouth_img.src = path + "mouth/woman/mouth" + mouth_nbr;
+                nose_img.src = path + "nose/nose" + nose_nbr;
+                fronthair_img.src = path + "fronthair/man/fronthair" + fronthair_nbr;
+            } else (select_boy.onclick == true) {
+                backhair_img.src = path + "backhair/man/backhair" + backhair_nbr;
+                body_img.src = path + "body/body" + body_nbr;
+                clothes_img.src = path + "clothes/clothes" + clothes_nbr;
+                eyebrows_img.src = path + "eyebrows/eyebrows" + eyebrows_nbr;
+                eyes_img.src = path + "eyes/woman/eyes" + eyes_nbr;
+                mouth_img.src = path + "mouth/man/mouth" + mouth_nbr;
+                nose_img.src = path + "nose/nose" + nose_nbr;
+                fronthair_img.src = path + "fronthair/man/fronthair" + fronthair_nbr;
+                };
+        };       eyebrows_img.crossOrigin = "anonymous";
+        eyes_img.crossOrigin = "anonymo    ries" + accessories_nbr;                
+                fronthair_img.src = path + "fronthair/man/fronthair" + fronthair_nbr;
 
         let background_color = get_random_color();
 
