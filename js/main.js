1b83c71c4decc6ee07ba098d233e0a8c308838e3
CappelleJess <cappellejessica.lcj@gmail.com>
2022-03-18 12:41:39 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -27,8 +27,8 @@ document.addEventListener("DOMContentLoaded", function() {
         gender = gender + "/";
         console.log(gender);
 
-        window.mySelect =  function() {
-        var drop = document.getElementById("dropMenu");
+        window.mySelect =  function(id) {
+        var drop = document.getElementById('id');
             if (drop.className.indexOf("w3-show") == -1) {
                 drop.className += " w3-show";
                 drop.previousElementSibling.className += " w3-black";
