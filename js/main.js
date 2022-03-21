c0962f7d1319996e1928d801ac344d7c95cac83d
CappelleJess <cappellejessica.lcj@gmail.com>
2022-03-21 17:39:32 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -27,7 +27,7 @@ document.addEventListener("DOMContentLoaded", function() {
         gender = gender + "/";
         console.log(gender);
 
-        function openSelect = (id) => {
+        function openSelect(id) {
             var x = document.getElementsByClassName("class");
             for (i = 0; i < x.length; i++) {
                 x[i].style.display = "none";  
