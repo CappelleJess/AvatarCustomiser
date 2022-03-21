2870e3f6ec1b048957e6abb6c9a6499b9a12b24d
CappelleJess <cappellejessica.lcj@gmail.com>
2022-03-21 11:47:04 +0100

    up

--- js/main.js
+++ js/main.js
@@ -27,11 +27,13 @@ document.addEventListener("DOMContentLoaded", function() {
         gender = gender + "/";
         console.log(gender);
 
-        const accordion = document.getElementsByClassName('w3-sidebar w3-bar-block w3-light-grey w3-card');
-        for (i = 0; i<accordion.length; i++) {
-            accordion[i].addEventListener('click', function() {
-                this.classList.toggle('active')
-            })
+        function myFunction(id) {
+            var x = document.getElementById(id);
+            if (x.className.indexOf("w3-show") == -1) {
+                x.className += " w3-show";
+            } else { 
+                x.className = x.className.replace(" w3-show", "");
+            }
         }
 
         let matched = get_random_image(35, 1);
