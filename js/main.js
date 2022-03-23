be753923755471be2e9abb24f952fddd50579b43
CappelleJess <cappellejessica.lcj@gmail.com>
2022-03-23 13:53:49 +0100

    up

--- js/main.js
+++ js/main.js
@@ -29,13 +29,13 @@ document.addEventListener("DOMContentLoaded", function() {
         gender = gender + "/";
         console.log(gender);
 
-        window.openSelect = function(id) {
-            var x = document.getElementsByClassName("class");
-            for (i = 0; i < x.length; i++) {
-                x[i].style.display = "none";  
-            }
-        document.getElementById(id).style.display = "block";  
-        };
+        // window.openSelect = function(id) {
+        //     var x = document.getElementsByClassName("class");
+        //     for (i = 0; i < x.length; i++) {
+        //         x[i].style.display = "none";  
+        //     }
+        // document.getElementById(id).style.display = "block";  
+        // };
 
         window.mySelect = function(id) {
             var x = document.getElementById(id);
