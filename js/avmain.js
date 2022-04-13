1c87d08769e5fe35dee051f33b36831a7094d51c
CappelleJess <cappellejessica.lcj@gmail.com>
2022-04-13 12:23:58 +0200

    Update avmain.js

--- js/avmain.js
+++ js/avmain.js
@@ -13,4 +13,27 @@ document.addEventListener("DOMContentLoaded", function() {
 
     function setup() {
         const ctx = canvas.getElementById('2d');
+        const download_btn = document.getElementById('download_btn');
         const path = "img/preset/";
+        const queryString = window.location.search;
+        const params = new URLSearchParams(queryString);
+        let gender = params.get('gender');
+        if (gender != 'man') gender = 'woman';
+        gender = gender + "/";
+        console.log(gender);
+
+        let woman_nbr = get_random_image(25, 1);
+        let man_nbr = get_random_image (7, 1);
+
+        let woman_img = new Image();
+        let man_img = new Image();
+
+        woman_img.crossOrigin = "anonymous";
+        man_img.crossOrigin = "anonymous";
+
+        woman_img.src = path + gender + "woman/woman" + woman_nbr;
+        man_img.src = path + gender + "man/man" + man_nbr;
+
+        window.onload = function() {
+            draw_avatar();
+        }
