49cb13b34cba5a0f6e951e446ec870aa77caf014
CappelleJess <cappellejessica.lcj@gmail.com>
2022-04-14 13:45:55 +0200

    Update avmain.js

--- js/avmain.js
+++ js/avmain.js
@@ -1,5 +1,12 @@
 var random_images_array = ["", "",];
 
+const queryString = window.location.search;
+        const params = new URLSearchParams(queryString);
+        let gender = params.get('gender');
+        if (gender != 'man') gender = 'woman';
+        gender = gender + "/";
+        console.log(gender);
+
 function getRandomImage(imgAr, path) {
   path = path || 'img/preset/';
   var num = Math.floor(Math.random() * imgAr.length);
