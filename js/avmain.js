7eb80aa358c2405b560e6c9f55dd14ce4d3d62b4
CappelleJess <cappellejessica.lcj@gmail.com>
2022-04-14 11:58:34 +0200

    Update avmain.js

--- js/avmain.js
+++ js/avmain.js
@@ -1,12 +1,13 @@
 let getRandomImage = () => {
-              var images = new Array();
+  var images = new Array();
 
-            var randomNum1 = Math.floor(Math.random() * images.length);
-            var randomNum2 = Math.floor(Math.random() * images.length);
+  var randomNum1 = Math.floor(Math.random() * images.length);
+  var randomNum2 = Math.floor(Math.random() * images.length);
 
-            if (randomNum1 == randomNum2) {
-                return getRandomImage();
-            }
-            document.getElementById("firstPic").src = images[randomNum1];
-            document.getElementById("secondPic").src = images[randomNum2];
-        };
+    if (randomNum1 == randomNum2) {
+      return getRandomImage();
+    }
+
+  document.getElementById("").src = images[randomNum1];
+  document.getElementById("").src = images[randomNum2];
+};
