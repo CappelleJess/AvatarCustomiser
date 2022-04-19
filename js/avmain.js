aa75ddffd0c79b9b335b9da40a497bb7cc293218
CappelleJess <cappellejessica.lcj@gmail.com>
2022-04-19 16:06:29 +0200

    Update avmain.js

---
+++ js/avmain.js
@@ -0,0 +1,6 @@
+window.onload = function ()
+
+var woman = new Image();
+      var woman_nbr = Math.floor(Math.random()*35)+1;
+      var woman_nm = "img/preset/woman/woman" + woman_nbr + ".png";
+      woman.src = woman_nm;
