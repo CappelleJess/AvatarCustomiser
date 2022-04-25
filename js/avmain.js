85021c2d8dfea3f888fe6b7384e8a1eb404b3752
CappelleJess <cappellejessica.lcj@gmail.com>
2022-04-25 16:50:44 +0200

    Update avmain.js

--- js/avmain.js
+++ js/avmain.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", function() {
             }
         });
 
-    let canvases = document.getElementsByTagName('canvas');
+    const canvases = document.getElementsByTagName('canvas');
 
     if (canvases.getContext) {
         const ctx = canvases.getContext('2d');
